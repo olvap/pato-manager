f71d3934549502f9af3087806f3815ba4a998e40
Pablo Oldani <oldani.pablo@gmail.com>
2014-11-04 10:24:57 -0300

    Add test for tasks.

--- tests/unit/task_test.rb
+++ tests/unit/task_test.rb
@@ -1,8 +1,28 @@
 require './tests/test_helper'
 
+
 scope do
+
+  setup do
+    @task = Task.create name: 'Learn Angular'
+  end
+
   test 'save task' do
-    Task.create name: 'first task'
     assert Task.all.count == 1
   end
+
+  test 'a task must be on "todo" state when is created' do
+    assert @task.state == 'todo'
+  end
+
+  test '#todos should show all the task whit todo state' do
+    assert Task.todos.count == 1
+  end
+
+  test '#done! should set a task as done' do
+    @task.done!
+    assert task.state == 'done'
+    assert Task.todos.count == 0
+  end
+
 end
